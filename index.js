48589aadc847c61c9759d6111ac304abecb26abc
coder71 <kamrunnaher877@gmail.com>
2022-01-27 03:01:29 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
 
 // INITIALIZE EXPRESS APP
 const app = express();
@@ -11,6 +12,24 @@ const PORT = process.env.PORT || 5000;
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors());
+
+//connetion URI of mongodb
+const uri = process.env.MONGODB_URI;
+
+// connect to mongodb database
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('welcome to trellas [A travel sharing website]');
