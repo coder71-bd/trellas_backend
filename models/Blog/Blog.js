f5a8f44a75fd0aff25ebf0664195db62bd105fdf
coder71 <kamrunnaher877@gmail.com>
2022-01-27 17:50:30 +0600

    blog model added

---
+++ models/Blog/Blog.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    time: {
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('Blog', BlogSchema);
